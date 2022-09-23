81ca8354f3826fb31e25d50a9fa050420e251fd3
nyancathulu <112297007+nyancathulu@users.noreply.github.com>
2022-09-23 15:24:45 -0700

    fps movement done, need models

--- fixedboatgame/Assets/fpsmovement.cs
+++ fixedboatgame/Assets/fpsmovement.cs
@@ -6,6 +6,13 @@ public class fpsmovement : MonoBehaviour
 {
     public CharacterController charcontroller;
     public float speed;
+    public float gravity = -9.81f;
+    Vector3 velocity;
+    public Transform groundcheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float jumpheight = 3f;
+    bool grounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +22,25 @@ public class fpsmovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+        grounded = Physics.CheckSphere(groundcheck.position, groundDistance, groundMask);
+
+        if (grounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpheight * -2 * gravity);
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
-        charcontroller.Move(move * speed * Time.deltaTime);
+        charcontroller.Move(Vector3.Normalize(move) * speed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        charcontroller.Move(velocity * Time.deltaTime);
     }
 }
