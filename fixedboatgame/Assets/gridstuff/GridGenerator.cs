05f7600d6f3dca7bd26dddb40e54869ffc37963c
nyancathulu <112297007+nyancathulu@users.noreply.github.com>
2022-09-26 17:19:19 -0700

    discovered some neat parenting properties for the tiles

--- fixedboatgame/Assets/gridstuff/GridGenerator.cs
+++ fixedboatgame/Assets/gridstuff/GridGenerator.cs
@@ -16,6 +16,7 @@ public class GridGenerator : MonoBehaviour
     GameObject fart;
     GameObject pee;
     // Start is called before the first frame update
+    public List<GameObject> oldtiles = new List<GameObject>();
     void Start()
     {
         gridinstantiate = true;
@@ -28,15 +29,18 @@ public class GridGenerator : MonoBehaviour
         {
             
             GameObject initialinstante = Instantiate(gridprefab, new Vector3(Player.position.x, 0, Player.position.z), Quaternion.identity);
+            oldtiles.Add(initialinstante);
             for (int j = 0; j < Mathf.Floor(rowLength / 2); j++)
             {
                 GameObject gawdam = Instantiate(gridprefab, new Vector3(initialinstante.transform.position.x, 0, initialinstante.transform.position.z + zSpace + (zSpace * (j % rowLength))), Quaternion.identity);
                 gawdam.transform.parent = parent.transform;
+                oldtiles.Add(gawdam);
             }
             for (int j = 0; j < Mathf.Floor(rowLength / 2); j++)
             {
                 GameObject gawda = Instantiate(gridprefab, new Vector3(initialinstante.transform.position.x, 0, initialinstante.transform.position.z - zSpace - (zSpace * (j % rowLength))), Quaternion.identity);
                 gawda.transform.parent = parent.transform;
+                oldtiles.Add(gawda);
             }
             initialinstante.transform.parent = parent.transform;
             for (int i=0; i<Mathf.Floor(columnLength/2); i++)
@@ -46,13 +50,16 @@ public class GridGenerator : MonoBehaviour
                 {
                     GameObject shsh = Instantiate(gridprefab, new Vector3(fart.transform.position.x, 0, fart.transform.position.z + zSpace + (zSpace * (j % rowLength))), Quaternion.identity);
                     shsh.transform.parent = parent.transform;
+                    oldtiles.Add(shsh);
                 }
                 for (int j = 0; j < Mathf.Floor(rowLength / 2); j++)
                 {
                     GameObject sh = Instantiate(gridprefab, new Vector3(fart.transform.position.x, 0, fart.transform.position.z - zSpace - (zSpace * (j % rowLength))), Quaternion.identity);
                     sh.transform.parent = parent.transform;
+                    oldtiles.Add(sh);
                 }
                 fart.transform.parent = parent.transform;
+                oldtiles.Add(fart);
             }
             for (int i = 0; i < Mathf.Floor(columnLength / 2); i++)
             {
@@ -61,13 +68,16 @@ public class GridGenerator : MonoBehaviour
                 {
                     GameObject huhuh = Instantiate(gridprefab, new Vector3(pee.transform.position.x, 0, pee.transform.position.z - zSpace - (zSpace * (j % rowLength))), Quaternion.identity);
                     huhuh.transform.parent = parent.transform;
+                    oldtiles.Add(huhuh);
                 }
                 for (int j = 0; j < Mathf.Floor(rowLength / 2); j++)
                 {
                     GameObject dududn = Instantiate(gridprefab, new Vector3(pee.transform.position.x, 0, pee.transform.position.z + zSpace + (zSpace * (j % rowLength))), Quaternion.identity);
                     dududn.transform.parent = parent.transform;
+                    oldtiles.Add(dududn);
                 }
                 pee.transform.parent = parent.transform;
+                oldtiles.Add(pee);
             }
             gridinstantiate = false;
         }
--- fixedboatgame/Assets/gridstuff/chunker.cs
+++ fixedboatgame/Assets/gridstuff/chunker.cs
@@ -4,19 +4,48 @@ using UnityEngine;
 
 public class chunker : MonoBehaviour
 {
+    GameObject currentchunk;
+    bool ahaha;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ahaha = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (ahaha == true)
+        {
+            currentchunk = GameObject.FindWithTag("Grid");
+            Debug.Log("whatehec");
+            ahaha = false;
+        }
+        if (GameObject.FindWithTag("Grid") != currentchunk)
+        {
+            Debug.Log("gaws ");
+            ahaha = true;
+        }
         
     }
-    private void OnTriggerEnter(Collider other)
+    /*private void OnTriggerStay(Collider other)
     {
+        if (other.gameObject.name == "gridprefab(Clone)")
+        {
+            Debug.Log("ahagahaga");
+            if (ahaha == true)
+            {
+                currentchunk = other.gameObject;
+                Debug.Log("whatehec");
+                ahaha = false;
+            }
+            if (other.gameObject != currentchunk)
+            {
+                Debug.Log("gaws ");
+                ahaha = true;
+            }
+        }
         
-    }
+        
+    }*/
 }
